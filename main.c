0e307b7cdf18730571b3fc2a4dcb1ce188ceb59f
Bridget Hartin Pasco <isaacpasco@icloud.com>
2024-05-27 15:04:43 -0700

    initial commit

---
+++ main.c
@@ -0,0 +1,75 @@
+#include <raylib.h>
+#include <raymath.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+void update_positions(float * thetas, float * radii,float * zs, Matrix * buffer, const int num_stars){
+    for(int i =0; i<num_stars; i++){
+        float radius = radii[i];
+        float theta = thetas[i];
+        float dtheta =  2/sqrt(radius+1);
+        theta +=dtheta*GetFrameTime();
+        thetas[i] = theta;
+        buffer[i] = MatrixTranslate(radius*cos(theta),radius*sin(theta), zs[i]);
+    }
+}
+int main(void){
+    const int num_stars = 32000;
+    Matrix stars_trans[num_stars] = {};
+    float stars_thetas[num_stars] = {};
+    float stars_radii[num_stars] = {};
+    float stars_z[num_stars] = {};
+    unsigned int occupied[num_stars/32] = {};
+    int true_count = 0;
+    int false_count = 0;
+    srand(time(0));
+    for(int i =0; i<num_stars; i++){
+        float radius = (rand()%3200+(1));
+        radius *= radius;
+        radius /= 13653;
+        float theta = rand()%(int)(2*PI*1000);
+        float z= rand()%100+800;
+        theta = theta/1000;
+        stars_thetas[i] = theta;
+        stars_radii[i] = radius;
+        stars_z[i] = z;
+        stars_trans[i] = MatrixTranslate(radius*cos(theta),radius*sin(theta),z);
+        int idx = i/32;
+        int offset = i%32;
+        int v_act = (rand()%10) == 0;
+        if(v_act){
+            true_count++;
+        } else{
+            false_count++;
+        }
+        int value = v_act <<(31-offset);
+        occupied[idx]|= value;
+    }
+    printf("true count: %d. false count:%d", true_count, false_count);
+    SetTraceLogLevel(LOG_ERROR);
+    InitWindow(1000, 1000,"Hello Sailor");
+    SetTargetFPS(61);
+    Material m = LoadMaterialDefault();
+    m.shader = LoadShader("shaders/instance.vs", "shaders/instance.fs");
+    m.shader.locs[SHADER_LOC_MATRIX_MVP] = GetShaderLocation(m.shader, "mvp");
+    m.shader.locs[SHADER_LOC_VECTOR_VIEW] = GetShaderLocation(m.shader, "viewPos");
+    m.shader.locs[SHADER_LOC_MATRIX_MODEL] = GetShaderLocationAttrib(m.shader, "instanceTransform");
+    int buffer = GetShaderLocation(m.shader, "occupied");
+    SetShaderValueV(m.shader, buffer, occupied, SHADER_UNIFORM_INT, num_stars/32);
+    m.maps[MATERIAL_MAP_DIFFUSE].color = RED;
+    Mesh mesh  = GenMeshSphere(3, 5, 5);
+    Camera3D cam = {.fovy = 80, .position = {0,0,-100}, .projection = CAMERA_PERSPECTIVE, .target = {0,0,-1}, .up = {0,1,0}};
+    Matrix trans = MatrixTranslate(0,0, 100);
+    m.maps[MATERIAL_MAP_DIFFUSE].color =WHITE;
+    while (!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground(BLACK);
+        BeginMode3D(cam);
+        DrawMeshInstanced(mesh, m, stars_trans, num_stars);
+        EndMode3D();
+        DrawFPS(940, 60);
+        update_positions(stars_thetas, stars_radii, stars_z,stars_trans, num_stars);
+        EndDrawing();
+    } 
+    return 0;
+}
---
+++ shaders/base.fs
@@ -0,0 +1,27 @@
+#version 330
+
+// Input vertex attributes (from vertex shader)
+in vec2 fragTexCoord;
+in vec4 fragColor;
+
+// Input uniform values
+uniform sampler2D texture0;
+uniform vec4 colDiffuse;
+
+// Output fragment color
+out vec4 finalColor;
+
+// NOTE: Add here your custom variables
+
+void main()
+{
+    // Texel color fetching from texture sampler
+    vec4 texelColor = texture(texture0, fragTexCoord);
+
+    // NOTE: Implement here your fragment shader code
+
+    finalColor.r = 1;
+    finalColor.g = 1; 
+    finalColor.b = 1;
+    finalColor.a = 1;
+}
